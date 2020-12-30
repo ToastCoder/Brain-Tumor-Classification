6e821ab491459e5c3b65849d48d10e6720087944
ToastCoder <vicky.pcbasic@gmail.com>
2020-12-30 20:54:34 +0530

    Some changes

--- train.py
+++ train.py
@@ -1,3 +1,15 @@
+# BRAIN TUMOR CLASSIFICATION
+
+# FILE NAME: train.py
+
+# DEVELOPED BY: Vigneshwar Ravichandar
+
+# TOPICS: Multi-Class Classification, Deep Learning, TensorFlow, Convolutional Neural Networks
+
+# DISABLE TF DEBUGGING INFORMATION
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 # IMPORTING REQUIRED LIBRARIES
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,37 +33,36 @@ test_data = datagen_test.flow_from_directory(dir_test,
                                                class_mode = 'binary', 
                                                batch_size = 10)
 
-# DEFINING THE NEURAL NETWORK
-model = tf.keras.models.Sequential()
+# FUNCTION FOR NEURAL NETWORK
+def tumorModel():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D( input_shape = (200,200,1), activation = 'relu', filters = 32, kernel_size = (5,5) ))
+    model.add(tf.keras.layers.MaxPool2D( pool_size = 2, strides = 2 ))
+    model.add(tf.keras.layers.Dropout(rate= 0.25))
+    model.add(tf.keras.layers.Conv2D( filters = 64, kernel_size = (3,3), activation = 'relu'))
+    model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
+    model.add(tf.keras.layers.Dropout(rate= 0.25))
+    model.add(tf.keras.layers.Conv2D( filters = 128, kernel_size = (3,3), activation = 'relu'))
+    model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
+    model.add(tf.keras.layers.Dropout(rate= 0.25))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(256, activation = 'relu'))
+    model.add(tf.keras.layers.Dense(4, activation = 'softmax'))
+    return model
 
-model.add(tf.keras.layers.Conv2D( input_shape = (200,200,1),
-                                  activation = 'relu',
-                                  filters = 32,
-                                  kernel_size = 3 ))
+ACC_THRESHOLD = 0.99
+# CALLBACK CLASS
+class Callback(tf.keras.callbacks.Callback): 
+    def on_epoch_end(self, epoch, logs={}): 
+        if(logs.get('accuracy') > ACC_THRESHOLD):   
+            print("\nReached %2.2f%% accuracy, so stopping training!!" %(ACC_THRESHOLD*100))   
+            self.model.stop_training = True
 
-model.add(tf.keras.layers.MaxPool2D( pool_size = 2, strides = 2 ))
-
-model.add(tf.keras.layers.Conv2D( filters = 64,
-                                  kernel_size = 3,
-                                  activation = 'relu'))
-
-model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
-
-model.add(tf.keras.layers.Conv2D( filters = 128,
-                                  kernel_size = 3,
-                                  activation = 'relu'))
-
-model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
-
-model.add(tf.keras.layers.Flatten())
-
-model.add(tf.keras.layers.Dense(256, activation = 'relu'))
-
-model.add(tf.keras.layers.Dense(4, activation = 'softmax'))
-
+model = tumorModel()
+callback = Callback()
 # FITTING THE DATA AND TRAINING THE MODEL
 model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
-history = model.fit(train_data, validation_data = test_data, epochs = 10,verbose = 1)
+history = model.fit(train_data, validation_data = test_data, epochs = 30,verbose = 1, batch_size = 1,callbacks = [callback])
 
 # SAVING THE TRAINED MODEL
 tf.keras.models.save_model(model,'./model/tumor-model')
